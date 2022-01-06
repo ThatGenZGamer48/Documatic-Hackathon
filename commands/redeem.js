af66237160935db4c43b30f9e46173af77b8baa9
ThatGenZGamer48 <thatgenzgamer@gmail.com>
2022-01-06 19:14:03 +0530

    There are a few errors but we can fix them later

--- commands/redeem.js
+++ commands/redeem.js
@@ -3,16 +3,57 @@ const { MessageEmbed } = require("discord.js");
 const UserDetails = require("../models/UserDetails");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("redeem")
-		.setDescription("Redeem a random number of coins"),
-	async execute(interaction) {
-		const interactionUserDetails = await UserDetails.findByPk(
-			interaction.user.id
-		);
+    data: new SlashCommandBuilder()
+        .setName("redeem")
+        .setDescription("Redeem a random number of coins"),
+    async execute(interaction) {
+        const interactionUserDetails = await UserDetails.findByPk(
+            interaction.user.id
+        );
 
-		console.log(interactionUserDetails["latestRedeemedTime"]);
+        const latestRedeemedTime = interactionUserDetails["latestRedeemedTime"];
 
-		await interaction.reply({ content: "." });
-	},
+        const currentUTCDate = new Date();
+
+        const getTimeDifference = (dt2, dt1) => {
+            let timeDifference = (dt2.getTime() - dt1.getTime()) / 1000;
+            timeDifference /= 60;
+
+            return Math.abs(Math.round(timeDifference));
+        };
+
+        timeBetween = getTimeDifference(currentUTCDate, latestRedeemedTime);
+
+        randomCoins = Math.floor(Math.random() * (1000 - 100) + 100);
+
+        const getRandomCoins = () => {
+            return Math.floor(Math.random() * (1000 - 100) + 100);
+        };
+
+        if (timeBetween >= 20) {
+            randomCoins = getRandomCoins();
+
+            const currentCoins = interactionUserDetails["coins"];
+
+            const addedCoins = currentCoins + BigInt(randomCoins);
+
+            const updated = await UserDetails.update(
+                { coins: addedCoins, latestRedeemedTime: currentUTCDate },
+                {
+                    where: {
+                        userId: interaction.user.id,
+                    },
+                }
+            );
+
+            await interaction.reply({
+                content: `Added ${randomCoins} to your balance!`,
+            });
+        } else {
+            return interaction.reply({
+                content:
+                    "You cannot redeem now. It has not yet been 20 minutes since you redeemed last.",
+            });
+        }
+    },
 };
